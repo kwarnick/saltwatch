aceb2aa720b7f52cd86dfc9ed4652a7667899d8a
Keith Warnick <keith.h.warnick@gmail.com>
2015-03-18 05:08:10 +0000

    it's all coming together now

---
+++ saltfetch.py
@@ -0,0 +1,94 @@
+import requests
+import pickle
+import os
+
+STATE_JSON_URL = 'http://www-cdn-twitch.saltybet.com/state.json'
+PLAYERS_FILENAME = 'players.p'
+MATCHES_FILENAME = 'matches.p'
+
+#HEADERS = {
+#        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0'
+#    }
+
+HEADERS = {
+        'Host': 'www-cdn-twitch.saltybet.com:8000',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Referer': 'http://www.saltybet.com/',
+        'Origin': 'http://www.saltybet.com',
+        'Connection': 'keep-alive',
+}
+
+
+def load_persistent_data():
+    global player_id_dict, player_name_dict
+    global matches
+
+    if os.path.isfile(PLAYERS_FILENAME):
+        player_id_dict, player_name_dict = pickle.load(open(PLAYERS_FILENAME, 'rb'))
+    else:
+        player_id_dict, player_name_dict = {}, {}
+
+    if os.path.isfile(MATCHES_FILENAME):
+        matches = pickle.load(open(MATCHES_FILENAME, 'rb'))
+    else:
+        matches = []
+
+
+def save_persistent_data():
+    if player_id_dict and player_name_dict:
+        pickle.dump([player_id_dict, player_name_dict], open(PLAYERS_FILENAME, 'wb'))
+    else:
+        pickle.dump([{}, {}], open(PLAYERS_FILENAME, 'wb'))
+
+    if matches:
+        pickle.dump(matches, open(MATCHES_FILENAME, 'wb'))
+    else:
+        pickle.dump([], open(MATCHES_FILENAME, 'wb'))
+
+
+def get_state():
+    #s = requests.Session()
+    #s.headers.update(HEADERS)    
+    #r = s.get(STATE_JSON_URL)
+    r = requests.get(STATE_JSON_URL, headers=HEADERS)
+    state = r.json()
+
+    return state
+
+
+def process_state(state):
+    global matches
+    
+    # Check for notification of a winner
+    if state['status'] == u'1' or state['status'] == u'2':
+        ## Potentially include logic to identify game mode here first
+
+        p1_id = get_player_id_by_name(state['p1name'])  # ID for P1
+        p2_id = get_player_id_by_name(state['p2name'])  # ID for P2
+        winner = int(state['status'])-1                 # Winner - 0/1 for p1/p2
+        p1total = int(state['p1total'].replace(',','')) # $ bet on P1
+        p2total = int(state['p2total'].replace(',','')) # $ bet on P2
+        timestamp = int(time.time())                    # Timestamp (Unix epoch seconds)
+        last_match = [p1_id, p2_id, winner, p1total, p2total, timestamp]
+        matches.append(last_match)
+        print('Match saved: ', last_match) 
+
+
+def get_player_id_by_name( pname ):
+    global player_name_dict
+    global player_id_dict
+
+    # Keep dictionaries updated if name is new
+    if pname not in player_id_dict:
+        if player_id_dict:
+            new_id = max(player_id_dict.values())+1
+            player_id_dict[pname] = new_id
+            player_name_dict[new_id] = pname
+        else:
+            player_id_dict[pname] = 0
+            player_name_dict[0] = pname
+
+    return player_id_dict[pname]
--- saltwatch.py
+++ saltwatch.py
@@ -1,104 +1,73 @@
-import requests
-import pickle
-import os
+import websocket
+import time
+import sys
+try:
+    import httplib
+except ImportError:
+    import http.client as httplib 
+import saltfetch as sf
 
-STATE_JSON_URL = 'http://www-cdn-twitch.saltybet.com/state.json'
-PLAYERS_FILENAME = 'players.p'
-MATCHES_FILENAME = 'matches.p'
-
-HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0'
-    }
-"""
-Host: www-cdn-twitch.saltybet.com:8000
-User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-Accept-Language: en-US,en;q=0.5
-Accept-Encoding: gzip, deflate
-Referer: http://www.saltybet.com/
-Origin: http://www.saltybet.com
-Cookie: __cfduid=d6a0f1d27beecad87f73784ed9130a45b1426561551; _ga=GA1.2.1576583260.1426561550; _gat=1
-Connection: keep-alive
-
-"""
+# Global variables/params for timed behaviors
+last_state_check = time.time()
+last_data_write = time.time()
+STATE_CHECK_COOLDOWN = 2.    # Don't get state twice in a cooldown period - prevent duplicates
+DATA_WRITE_PERIOD = 1*60*60  # Save data to disk evey hour
 
 
-
-
-def load_persistent_data():
-    global player_id_dict, player_name_dict
-    global matches
-
-    if os.path.isfile(PLAYERS_FILENAME):
-        player_id_dict, player_name_dict = pickle.load(open(PLAYERS_FILENAME, 'rb'))
-    else:
-        player_id_dict, player_name_dict = {}, {}
-
-    if os.path.isfile(MATCHES_FILENAME):
-        matches = pickle.load(open(MATCHES_FILENAME, 'rb'))
-    else:
-        matches = []
-
-
-def save_persistent_data():
-    if player_id_dict and player_name_dict:
-        pickle.dump([player_id_dict, player_name_dict], open(PLAYERS_FILENAME, 'wb'))
-    else:
-        pickle.dump([{}, {}], open(PLAYERS_FILENAME, 'wb'))
-
-    if matches:
-        pickle.dump(matches, open(MATCHES_FILENAME, 'wb'))
-    else:
-        pickle.dump([], open(MATCHES_FILENAME, 'wb'))
-
-
-def get_state():
-    s = requests.Session()
-    s.headers.update(HEADERS)    
-    r = s.get(STATE_JSON_URL)
-    state = r.json()
-
-    return state
-
-
-def process_state(state):
-    global matches
-
-    if state['status'] == u'1' or state['status'] == u'2':
-        p1_id = get_player_id_by_name(state['p1name'])
-        p2_id = get_player_id_by_name(state['p2name'])
-        p1total = int(state['p1total'].replace(',',''))
-        p2total = int(state['p2total'].replace(',',''))
-        winner = int(state['status'])
-        last_match = [p1_id, p2_id, winner, p1total, p2total]
-        matches.append(last_match)
-        print('Match saved: ', last_match) 
-
-
-
-
-def get_player_id_by_name( pname ):
-    global player_name_dict
-    global player_id_dict
-
-    # Keep dictionaries updated if name is new
-    if pname not in player_id_dict:
-        if player_id_dict:
-            new_id = max(player_id_dict.values())+1
-            player_id_dict[pname] = new_id
-            player_name_dict[new_id] = pname
-        else:
-            player_id_dict[pname] = 0
-            player_name_dict[0] = pname
-
-    return player_id_dict[pname]
-
-
-#def connect_to_
-
-def predict_winner(p1_id, p2_id):
-    return oracle.predict(p1_id, p2_id)
-
-# def get_best_bet(prediction) # Do we have the odds? The money bet on each player? Anything?
+def on_message(ws, message):
+    print(message)
+    global last_state_check, last_data_write
     
+    # Check if we're due to save data to disk
+    if time.time() - last_data_write > DATA_WRITE_PERIOD:
+        sf.save_persistent_data()
+        last_data_write = time.time()
+    
+    # Echo keepalive message
+    if message == u'2::':
+        ws.send(message)
+    # Handle state change notifications, which sometimes come in redundant pairs
+    if message == u'3::':
+        if time.time() - last_state_check > STATE_CHECK_COOLDOWN:
+            state = sf.get_state()
+            last_state_check = time.time()
+            print(state)
+            sf.process_state(state)
 
+
+def on_error(ws, error):
+    print(error)
+
+
+def on_close(ws):
+    sf.save_persistent_data()
+    print("### closed ###")
+
+
+def on_open(ws):
+    print("### open ###")
+
+
+def connect(server, port):
+    print("connecting to: %s:%d" %(server, port))
+
+    conn  = httplib.HTTPConnection(server + ":" + str(port))
+    conn.request('POST','/socket.io/1/')
+    resp  = conn.getresponse()
+    hskey = resp.read().decode('utf-8').split(':')[0]
+
+    ws = websocket.WebSocketApp(
+                    'ws://'+server+':'+str(port)+'/socket.io/1/websocket/'+hskey,
+                    on_open   = on_open,
+                    on_message = on_message,
+                    on_close = on_close)
+    
+    return ws
+
+
+if __name__ == "__main__":
+    sf.load_persistent_data()
+
+    websocket.enableTrace(True)
+    ws = connect('www-cdn-twitch.saltybet.com', 8000)
+    ws.run_forever()
